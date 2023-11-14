241dbd6ab3925201c2106290f7a6294fd49ccded
azhar-msc <149996464+azhar-msc@users.noreply.github.com>
2023-11-14 11:12:47 +0530

    initial commit

---
+++ dart/main.dart
@@ -0,0 +1,24 @@
+void main() { //entry point for dart
+
+  print("Hello"); //show output in console
+}
+
+//syntax => is a structure
+
+//brackets:
+
+// () => functional / arg / parameterized  bracket
+// {} => body / statement / flower bracket
+// [] => array / list / of bracket
+// <> => generic bracket
+// ' ' => single quote /string literals
+// " " => double quote /string literals
+// ; => end of  statement / end of line
+
+//Datatypes:
+//number: num => store all type of numbers(1, 2.5)
+//integer: int => store non-decimal numbers(1,90)
+//double: double => store decimal numbers(1.0, 2.5)
+//boolean: bool => store true/false
+//void: void => store nothing
+//string: String => store anything between string literals ('1','asdsd')

e75745c26263a9705a016ec89aeeaf8b7fcce8cd
azhar-msc <149996464+azhar-msc@users.noreply.github.com>
2023-11-26 11:07:44 +0530

    indexOf

--- dart/_task/task3.dart
+++ dart/_task/task3.dart
@@ -8,5 +8,30 @@
  */
 
 void main() {
-  
+  //1. Create a student list name('Alex','Bob', 'Clara'), list age(14, 15, 16), list class(9,10,11)
+
+  List<String> studentNames = ['Alex', 'Bob', 'Clara'];
+  List<int> studentAges = [14, 15, 16];
+  List<int> studentClasses = [9, 10, 11];
+
+  //2. fetch Bob age and print(Bob is 'x' years old)
+  int bobIndex = studentNames.indexOf('Bob'); //2
+  String bobName = studentNames[bobIndex];
+  int bobAge = studentAges[bobIndex];
+  print('$bobName is $bobAge years old');
+
+  //4. Add david name with smilar age as bob and class as alex
+  int alexIndex = studentNames.indexOf('Alex');
+  int alexCls = studentClasses[alexIndex];
+
+  studentNames.add('David');
+  studentAges.add(bobAge);
+  studentClasses.add(alexCls);
+
+  print(studentNames);
+  print(studentAges);
+  print(studentClasses);
+
+  //Methods of Lis
+  // length, add, addAll, remove(all kind of remove), indexOf.
 }

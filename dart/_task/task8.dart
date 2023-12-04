f81ce526f94c73416a54e9b3a5fe4e1cd2328f2b
azharmarmu <marmuazhar@gmail.com>
2023-12-04 19:43:41 +0530

    while loop

---
+++ dart/_task/task8.dart
@@ -0,0 +1,5 @@
+/**
+ * print 10-0 using while loop
+ * 
+ * 
+ */
---
+++ dart/while.dart
@@ -0,0 +1,13 @@
+void main() {
+  int i = 0;
+
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+}
+
+
+
+// i=i+1 => i++, ++i
+// i=i-1 => i--, --i

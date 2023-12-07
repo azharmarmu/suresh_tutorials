6c97c0fcd948bd027da9877772c50501175f7961
azharmarmu <marmuazhar@gmail.com>
2023-12-07 19:46:51 +0530

    debugger and task

---
+++ dart/_task/task11.dart
@@ -0,0 +1,6 @@
+//List of students(name, class, age, gender, favSport['football, vollyball'])
+//sports(footbal(m), coco(f), vollyball(f/m), chess(f/m), carrom(f/m))
+//List sports = [{'name': 'footbal', 'eligible': [M]}, {'name': 'vollyball', 'eligible': [M,F]}]
+//print stundent name who like football
+//ramya(F) is eligible for chess? true/false
+//print student avg age who playes carrom

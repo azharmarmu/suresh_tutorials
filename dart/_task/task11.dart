2f44e0ddfbe55c37442b96b08c0744ceba1558bf
azharmarmu <marmuazhar@gmail.com>
2023-12-08 19:04:39 +0530

    task 11

--- dart/_task/task11.dart
+++ dart/_task/task11.dart
@@ -4,3 +4,70 @@
 //print stundent name who like football
 //ramya(F) is eligible for chess? true/false
 //print student avg age who playes carrom
+
+void main() {
+//List of students(name, class, age, gender, favSport['football, vollyball'])
+  List<Map<String, dynamic>> students = [
+    {
+      'name': 'Pandian',
+      'class': 8,
+      'age': 13,
+      'gender': 'M',
+      'favSport': ['football'],
+    },
+    {
+      'name': 'Ramya',
+      'class': 9,
+      'age': 14,
+      'gender': 'F',
+      'favSport': ['vollyball'],
+    },
+  ];
+  List sports = [
+    {
+      'name': 'footbal',
+      'eligible': ['M']
+    },
+    {
+      'name': 'vollyball',
+      'eligible': ['M', 'F']
+    },
+    {
+      'name': 'chess',
+      'eligible': ['M', ]
+    },
+  ];
+
+  //Q1. print student name who like football
+  int i = 0;
+  while (i < students.length) {
+    bool checkFootball = students[i]['favSport'].contains('football');
+
+    if (checkFootball) {
+      print(students[0]['name']);
+    }
+    i++;
+  }
+
+
+
+  
+  List eligibleForChess = [];
+  for (int i = 0; i < sports.length; i++) {
+    bool checkChess = sports[i]['name'] == 'chess';
+    if (checkChess) {
+      eligibleForChess = sports[i]['eligible'];
+    }
+  }
+
+  for (int i = 0; i < students.length; i++) {
+    bool checkStudentName = students[i]['name'].toLowerCase() == 'ramya';
+    if (checkStudentName) {
+      var studentGender = students[i]['gender'];
+      print(eligibleForChess.contains(studentGender));
+    }
+  }
+}
+
+// 
+// ['football'] == 'football';

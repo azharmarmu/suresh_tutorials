56fefd4d367d7baccdf23dd2f2f318aee7c61615
azharmarmu <marmuazhar@gmail.com>
2023-12-14 10:43:11 +0530

    class with multiple objects

--- dart/_task/task15.dart
+++ dart/_task/task15.dart
@@ -6,3 +6,42 @@
  *  -Data members/data definition: name, age, city, father-name, group(school group like A1,A2,C)
  *  -Member functions/Method: getStudentNameAndAge, getAllDetails
  */
+
+void main() {
+  CollegeApplication s1 = CollegeApplication(
+      name: 'Alex', group: 'A1', city: 'Mumbai', fatherName: 'Watson', age: 20);
+  s1.getStudentNameAndAge();
+
+  CollegeApplication s2 = CollegeApplication(
+      name: 'Box', group: 'C', city: 'Chennai', fatherName: 'Samson', age: 19);
+
+  s2.getStudentNameAndAge();
+}
+
+class CollegeApplication {
+  late String name;
+  late int age;
+  late String city;
+  late String fatherName;
+  late String group;
+
+  CollegeApplication({
+    required this.name,
+    required this.age,
+    required this.city,
+    required this.fatherName,
+    this.group = 'C',
+  });
+
+  void getStudentNameAndAge() {
+    print('Name: $name\t Age: $age');
+  }
+
+  getAllDetails() {
+    print('Name: $name');
+    print('Age: $age');
+    print('City: $city');
+    print('Father Name: $fatherName');
+    print('Group: $group');
+  }
+}
---
+++ dart/_task/task16.dart
@@ -0,0 +1,8 @@
+/**
+ * 
+ * Tuition
+ * 
+ * Tutor(2): name, gender, age, subjects[]
+ * Student(4): name, gender, age, subjects[],
+ * 
+ */

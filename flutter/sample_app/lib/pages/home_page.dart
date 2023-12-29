08ee5dc0beeb62f742c4192f81c7894a347cee87
azharmarmu <marmuazhar@gmail.com>
2023-12-29 10:05:21 +0530

    Orientation/LayoutBuilder Builder

--- flutter/sample_app/lib/pages/home_page.dart
+++ flutter/sample_app/lib/pages/home_page.dart
@@ -8,13 +8,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.85),
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        title: const Text('Title'),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.blue,
+      //   centerTitle: true,
+      //   title: const Text('Title'),
+      // ),
       // body: ColumnWidget(),
-      body: const CalculatorWidget(),
+      body: OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+        return orientation == Orientation.landscape
+            ? const CalculatorWidget()
+            : LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  print('Screen width: ${constraints.maxWidth}');
+                  if (constraints.maxWidth <= 600) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    );
+                  } else if (constraints.maxWidth <= 800) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    );
+                  } else if (constraints.maxWidth > 800) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
+              );
+      }),
     );
   }
 }

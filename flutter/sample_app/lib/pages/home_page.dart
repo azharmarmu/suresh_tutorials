bc65d185fbed1d790f01261e37155512db20087e
azharmarmu <marmuazhar@gmail.com>
2024-01-08 10:34:11 +0530

    Listview

--- flutter/sample_app/lib/pages/home_page.dart
+++ flutter/sample_app/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sample_app/pages/tasks/calculator.dart';
+
+import 'widgets/listview_builder.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,42 +9,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.85),
-      // appBar: AppBar(
-      //   backgroundColor: Colors.blue,
-      //   centerTitle: true,
-      //   title: const Text('Title'),
-      // ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: false,
+        title: Expanded(child: Text('Title')),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
       // body: ColumnWidget(),
-      body: OrientationBuilder(
-          builder: (BuildContext context, Orientation orientation) {
-        return orientation == Orientation.landscape
-            ? const CalculatorWidget()
-            : LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  print('Screen width: ${constraints.maxWidth}');
-                  if (constraints.maxWidth <= 600) {
-                    return Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.red,
-                    );
-                  } else if (constraints.maxWidth <= 800) {
-                    return Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.green,
-                    );
-                  } else if (constraints.maxWidth > 800) {
-                    return Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.blue,
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
-              );
-      }),
+      body: ListViewBuilderExample(),
     );
   }
 }

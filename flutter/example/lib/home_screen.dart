aa18fdcff9e912cfcef53b7525894a1cd8fbd08a
azharmarmu <marmuazhar@gmail.com>
2024-01-16 10:19:09 +0530

    statefull widget lifecycle

--- flutter/example/lib/home_screen.dart
+++ flutter/example/lib/home_screen.dart
@@ -1,46 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/second_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    print('CreateState');
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int count = 0;
+  late int count = 0;
 
-  bool loginPage = true;
+  Color boxColor = Colors.red;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       appBar: AppBar(),
-      body: (loginPage)
-          ? Column(
-              children: [
-                const Text('Login '),
-                TextButton(
-                    onPressed: () {
-                      loginPage = false;
-                      setState(() {});
-                    },
-                    child: const Text('Go to Register'))
-              ],
-            )
-          : Column(
-              children: [
-                const Text('Register '),
-                TextButton(
-                  onPressed: () {
-                    loginPage = true;
-                    setState(() {});
-                  },
-                  child: const Text('Go to Login'),
+      body: Center(
+          child: Column(
+        children: [
+          Text('$count'),
+          const SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SecondScreen(),
                 ),
-              ],
-            ),
+              );
+            },
+            child: const Text('Move to Second Screen'),
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          count++;
+          setState(() {}); //rebuild the ui
+        },
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
   }
 }
 
